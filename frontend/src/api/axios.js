b10381383a34fafc6fd05264312ca36fa53092c8
vaidus2608m <vaidus2608m@gmail.com>
2026-06-04 10:29:27 +0530

    Finishing deploy settings

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
-    withCredentials: true,
-})
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1",
+  withCredentials: true,
+});
 
 export default API;
